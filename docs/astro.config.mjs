a90502ec10599a6cdc1ec7913180eeabff8394bf
Aaryn Tiger Malik <godofthemonki@gmail.com>
2025-10-21 17:47:31 -0400

    Fix path relativity

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://SlimifiedxD.github.io',
-	base: '/bedrock',
+	base: '/bedrock/',
 	integrations: [
 		starlight({
 			title: 'Bedrock',
