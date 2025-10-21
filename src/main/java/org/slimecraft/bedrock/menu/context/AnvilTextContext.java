60c7fb2ff7312bec9f0bcec6833106d75df2c6fb
Aaryn Tiger Malik <godofthemonki@gmail.com>
2025-10-21 11:47:01 -0400

    Document anvil rename behaviour

--- src/main/java/org/slimecraft/bedrock/menu/context/AnvilTextContext.java
+++ src/main/java/org/slimecraft/bedrock/menu/context/AnvilTextContext.java
@@ -10,6 +10,11 @@ public class AnvilTextContext extends PlayerContext {
         this.text = text;
     }
 
+    /**
+     * Get the text of the anvil. Be wary of the fact that if the text in the anvil
+     * is equal to that of the item's name, located in the left slot (index of 0),
+     * the return value will be an empty string.
+     */
     public String text() {
         return text;
     }
