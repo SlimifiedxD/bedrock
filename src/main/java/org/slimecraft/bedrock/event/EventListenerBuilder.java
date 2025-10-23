c317a2fdcbf3e42763c0951439bb812c31967777
--global <godofthemonki@gmail.com>
2025-10-23 11:28:07 -0400

    Remove unnecessary interface

--- src/main/java/org/slimecraft/bedrock/event/EventListenerBuilder.java
+++ src/main/java/org/slimecraft/bedrock/event/EventListenerBuilder.java
@@ -1,14 +1,9 @@
 package org.slimecraft.bedrock.event;
 
-import java.util.List;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
-public final class EventListenerBuilder<T>
-        implements
-        OptionalBuilder<T, EventListenerBuilder<T>>
-{
+public final class EventListenerBuilder<T> {
     private final EventListener<T> listener;
     private Filter<T> previousFilter;
 
@@ -16,7 +11,6 @@ public final class EventListenerBuilder<T>
         this.listener = new EventListener<>(eventClass);
     }
 
-    @Override
     public EventListenerBuilder<T> handler(Consumer<T> consumer) {
         EventHandler<T> handler;
         if (previousFilter != null) {
@@ -29,25 +23,21 @@ public final class EventListenerBuilder<T>
         return this;
     }
 
-    @Override
     public EventListenerBuilder<T> filter(Filter<T> filter) {
         previousFilter = filter;
         return this;
     }
 
-    @Override
     public EventListenerBuilder<T> filter(FilterBuilder<T> builder) {
         this.filter(builder.build());
         return this;
     }
 
-    @Override
     public EventListenerBuilder<T> filter(Predicate<T> predicate) {
         this.filter(new Filter<>(predicate));
         return this;
     }
 
-    @Override
     public EventListenerBuilder<T> filter(Predicate<T> predicate, Consumer<T> orElse) {
         final Filter<T> filter = new Filter<>(predicate);
         filter.setOrElse(orElse);
@@ -55,7 +45,6 @@ public final class EventListenerBuilder<T>
         return this;
     }
 
-    @Override
     public EventListener<T> build() {
         return listener;
     }
