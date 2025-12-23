8a9bffa104cd00129205f65acc38dfdd88f352a2
Aaryn Tiger Malik <godofthemonki@gmail.com>
2025-12-23 14:27:30 -0800

    Update titles in docs config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,28 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://SlimifiedxD.github.io',
-	base: '/bedrock/',
-	integrations: [
-		starlight({
-			title: 'Bedrock',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/SlimifiedxD/bedrock' }],
-			customCss: [
-				'./src/styles/theme.css'
-			],
-			sidebar: [
-				{
-					label: 'Introduction',
-					autogenerate: { directory: 'introduction' }
-				},
-				{
-					label: 'Cookbook',
-					autogenerate: { directory: 'cookbook' }
-				}
-			],
-		}),
-	],
+  site: "https://SlimifiedxD.github.io",
+  base: "/quartz/",
+  integrations: [
+    starlight({
+      title: "Quartz",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/SlimifiedxD/quartz",
+        },
+      ],
+      customCss: ["./src/styles/theme.css"],
+      sidebar: [
+        {
+          label: "Introduction",
+          autogenerate: { directory: "introduction" },
+        },
+        {
+          label: "Cookbook",
+          autogenerate: { directory: "cookbook" },
+        },
+      ],
+    }),
+  ],
 });
