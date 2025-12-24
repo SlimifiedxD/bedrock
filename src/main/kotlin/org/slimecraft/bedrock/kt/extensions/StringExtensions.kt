42276ad28c7a677eb07e4b863c840062276e2182
Aaryn Tiger Malik <godofthemonki@gmail.com>
2025-12-24 14:19:25 -0800

    Add support for different tags

--- src/main/kotlin/org/slimecraft/bedrock/kt/extensions/StringExtensions.kt
+++ src/main/kotlin/org/slimecraft/bedrock/kt/extensions/StringExtensions.kt
@@ -5,7 +5,13 @@ import net.kyori.adventure.text.minimessage.tag.Tag
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
 import org.slimecraft.bedrock.internal.Bedrock
 
-fun String.component(vararg args: Pair<String, Component>): Component {
-    val resolvers = args.map { TagResolver.resolver(it.first, Tag.selfClosingInserting { it.second }) }.toTypedArray()
+fun String.component(supplier: (value: Component) -> Tag, vararg args: Pair<String, Component>): Component {
+    val resolvers = args.map { TagResolver.resolver(it.first, supplier(it.second)) }.toTypedArray()
     return Bedrock.bedrock().miniMessage.deserialize(this, *resolvers)
+}
+
+fun String.component(vararg args: Pair<String, Component>): Component {
+    return this.component(supplier = { value ->
+        Tag.selfClosingInserting { value }
+    }, *args)
 }
