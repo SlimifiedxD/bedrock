d39e07a44b9df0c134d7eaf9fcfa927b7617a6fa
Aaryn Tiger Malik <godofthemonki@gmail.com>
2025-12-23 23:51:37 -0800

    Allow argument resolvers in kotlin-idiomatic way with #component() metohd

--- src/main/kotlin/org/slimecraft/bedrock/kt/extensions/StringExtensions.kt
+++ src/main/kotlin/org/slimecraft/bedrock/kt/extensions/StringExtensions.kt
@@ -1,8 +1,11 @@
 package org.slimecraft.bedrock.kt.extensions
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.tag.Tag
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
 import org.slimecraft.bedrock.internal.Bedrock
 
-fun String.component(): Component {
-    return Bedrock.bedrock().miniMessage.deserialize(this)
+fun String.component(vararg args: Pair<String, Component>): Component {
+    val resolvers = args.map { TagResolver.resolver(it.first, Tag.selfClosingInserting { it.second }) }.toTypedArray()
+    return Bedrock.bedrock().miniMessage.deserialize(this, *resolvers)
 }
